2c07d2c67f9cb562abfc4830c9c7ea37c2ce44d6
jongjin8976 <whdwls8976@gmail.com>
2023-05-10 14:01:03 +0900

    집에서 작업1

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -4,5 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		int num1 = 10;
 		System.out.println(num1);
+		System.out.println("집에서 작성~~");
+		System.out.println("집에 고구마 없다");
 	}
 }
