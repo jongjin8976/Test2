a33d6d294581fe95ed5850c19bc4123ec367de7d
jongjin8976 <whdwls8976@gmail.com>
2023-05-10 14:19:32 +0900

    test

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -6,5 +6,7 @@ public class Test2 {
 		System.out.println(num1);
 		System.out.println("집에서 작성~~");
 		System.out.println("집에 고구마 없다");
+		System.out.println("강의실에서 작성");
+		System.out.println("강의실에도 고구마는 없다");
 	}
 }
